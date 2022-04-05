12ee3981d0997cd8ee48c5b40dbb98083d86a6d8
Coihue555 <kevlar555@gmail.com>
2022-04-05 09:43:01 -0300

    todo por hoy

--- fl_chat_server/index.js
+++ fl_chat_server/index.js
@@ -22,7 +22,7 @@ const publicPath = path.resolve(__dirname, 'public');
 app.use( express.static(publicPath) );
 
 //Mis rutas
-app.use('api/login', require('./routes/auth') );
+app.use('/api/login', require('./routes/auth') );
 
 
 server.listen(process.env.PORT, (err)=>{
--- fl_chat_server/routes/auth.js
+++ fl_chat_server/routes/auth.js
@@ -1,12 +1,37 @@
-const { Router, response } = require('express');
+/*
+    path: api/login
+*/
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
 const router = Router();
 
-router.post('/new', (req, res = response)=>{
-    res.json({
-        ok: true,
-        msg: 'Crear usuario'
-    })
-})
+router.post('/new', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('email', 'El correo debe tener formato valido').isEmail(),
+    validarCampos
+],
+
+crearUsuario
+)
+
+
+router.post('/', [
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('email', 'El correo debe tener formato valido').isEmail(),
+    validarCampos
+    ],
+login
+)
+
+//validarJWT
+router.get('/renew', validarJWT, renewToken);
+
 
 
 module.exports = router;
